016fee8389a29a63bd508c5a70e03f944f4bcb49
nereac <n.corteslomana@gmail.com>
2013-01-06 13:27:08 +0100

    Apart 4: modificado paths

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -18,6 +18,14 @@ module NavigationHelpers
     when /^the new article page$/
       '/admin/content/new'
 
+    when /^the New Categories page$/
+      '/admin/categories/new'
+
+    when /^the edit '(.*)' category page$/
+      "/admin/categories/edit/#{Category.find_by_name($1).id}"
+
+
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
